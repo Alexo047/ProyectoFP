449fc027f5b4e880c25c6258a7820037e372875f
Alejandro Sanchez <asmcorreos@gmail.com>
2022-05-16 18:47:40 +0200

    Creado la página del IMC y el código (no funciona del todo) y descubierto como arreglar el problema de las imágenes (ImgButton)

--- app/src/main/java/com/example/proyectofp/Ajustes.java
+++ app/src/main/java/com/example/proyectofp/Ajustes.java
@@ -2,13 +2,26 @@ package com.example.proyectofp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Ajustes extends AppCompatActivity {
+
+    Button BirIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajustes);
+        BirIMC=findViewById(R.id.BotonirIMC);
+        BirIMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irIMC = new Intent(Ajustes.this, CalcularIMC.class);
+                startActivity(irIMC);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/proyectofp/CalcularIMC.java
@@ -0,0 +1,38 @@
+package com.example.proyectofp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalcularIMC extends AppCompatActivity {
+
+    Button BIMC;
+    EditText TextoALtura, TextoPeso;
+    TextView TextoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcular_imc);
+        BIMC=findViewById(R.id.BotonIMC);
+        TextoALtura=(EditText) findViewById(R.id.Altura);
+        TextoPeso=(EditText) findViewById(R.id.Peso);
+        TextoResultado=(TextView) findViewById(R.id.Resultado);
+        BIMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ValorAltura= Integer.parseInt(TextoALtura.getText().toString());
+                int ValorPeso= Integer.parseInt(TextoPeso.getText().toString());
+                int valorAlturaAlcuadrado= ValorAltura * ValorAltura;
+                int resulFinal = ValorPeso / valorAlturaAlcuadrado;
+                BIMC.setVisibility(View.GONE);
+                TextoResultado.setText("Su IMC es: "+ resulFinal);
+
+            }
+        });
+    }
+}
