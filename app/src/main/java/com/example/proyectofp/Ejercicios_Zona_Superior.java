20696ee579800e63179f79dcef6415fa9029dcb4
Alejandro Sanchez <asmcorreos@gmail.com>
2022-05-25 18:57:18 +0200

    Todo el código perfectamente creado de la zona superior inferior y cardio

--- app/src/main/java/com/example/proyectofp/Ejercicios_Zona_Superior.java
+++ app/src/main/java/com/example/proyectofp/Ejercicios_Zona_Superior.java
@@ -6,82 +6,122 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class Ejercicios_Zona_Superior extends AppCompatActivity {
-    private ImageView imagenCurl;
-    private ImageView imagenremocb;
-    private ImageView imagenbancaplano;
-    private ImageView imagenpress;
-    private ImageView imagenapertura;
-    private ImageView imagenpullover;
-    private ImageView imagenpressfrances;
-    private ImageView imagenDominada;
-    private ImageView imagenehpushup;
-    private ImageView imagenehpress;
-    private ImageView imagenremo;
+    private ImageButton imagenCurl;
+    private ImageButton imagenremocb;
+    private ImageButton imagenbancaplano;
+    private ImageButton imagenpress;
+    private ImageButton imagenapertura;
+    private ImageButton imagenpullover;
+    private ImageButton imagenpressfrances;
+    private ImageButton imagenDominada;
+    private ImageButton imagenehpushup;
+    private ImageButton imagenehpress;
+    private ImageButton imagenremo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicios_zona_superior);
-        imagenCurl=findViewById(R.id.imageView2);
-        /*imagenCurl.setOnClickListener(new View.OnClickListener() {
+        imagenCurl=findViewById(R.id.ImbCurl);
+        imagenremocb=findViewById(R.id.ImbRemoConBarra);
+        imagenbancaplano=findViewById(R.id.ImbPressbanca);
+        imagenpress=findViewById(R.id.ImbPress);
+        imagenapertura=findViewById(R.id.ImbApertura);
+        imagenpullover=findViewById(R.id.ImbPullover);
+        imagenpressfrances=findViewById(R.id.Imbpressfrances);
+        imagenehpushup=findViewById(R.id.ImbEhPush);
+        imagenDominada=findViewById(R.id.ImbDominadas);
+        imagenehpress=findViewById(R.id.ImbEhPress);
+        imagenremo=findViewById(R.id.ImbRemoAUnaMano);
+
+
+
+
+
+        imagenCurl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent biceps = new Intent(String.valueOf(Biceps.class));
+                Intent biceps = new Intent(Ejercicios_Zona_Superior.this, Biceps.class);
                 startActivity(biceps);
             }
-        });*/
+        });
+        imagenremocb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent remocb = new Intent(Ejercicios_Zona_Superior.this, Remo_con_barra.class);
+                startActivity(remocb);
+            }
+        });
+        imagenbancaplano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bancaplano = new Intent(Ejercicios_Zona_Superior.this, Pressbancaplano.class);
+                startActivity(bancaplano);
+            }
+        });
+        imagenpress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent press = new Intent(Ejercicios_Zona_Superior.this, Press.class);
+                startActivity(press);
+            }
+        });
+        imagenapertura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent apertura = new Intent(Ejercicios_Zona_Superior.this, Aperturabancoplano.class);
+                startActivity(apertura);
+            }
+        });
+        imagenpullover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pullover = new Intent(Ejercicios_Zona_Superior.this, Pulloverconmancuerna.class);
+                startActivity(pullover);
+            }
+        });
+        imagenpressfrances.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pressfrances = new Intent(Ejercicios_Zona_Superior.this, Pressfrancesmancuerna.class);
+                startActivity(pressfrances);
+            }
+        });
+        imagenDominada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dominada = new Intent(Ejercicios_Zona_Superior.this, Dominadas.class);
+                startActivity(dominada);
+            }
+        });
+
+        imagenehpushup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ehpushup = new Intent(Ejercicios_Zona_Superior.this, Ehpushups.class);
+                startActivity(ehpushup);
+            }
+        });
+        imagenehpress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ehpress = new Intent(Ejercicios_Zona_Superior.this, Ehpresshorizontalalternado.class);
+                startActivity(ehpress);
+            }
+        });
+
+        imagenremo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent remo = new Intent(Ejercicios_Zona_Superior.this, Remoaunamano.class);
+                startActivity(remo);
+            }
+        });
+
     }
 
-    public void biceps(View view) {
-        Intent biceps = new Intent(this, Biceps.class);
-        startActivity(biceps);
-    }
-
-
-    public void remoconbarra(View view) {
-        Intent remocb = new Intent(this, Remo_con_barra.class);
-        startActivity(remocb);
-    }
-
-    public void pressbancaplano(View view) {
-        Intent bancaplano = new Intent(this, Pressbancaplano.class);
-        startActivity(bancaplano);
-    }
-
-    public void press(View view) {
-        Intent press = new Intent(this, Press.class);
-        startActivity(press);
-    }
-    public void aperturabancoplano(View view) {
-        Intent apertura = new Intent(this, Aperturabancoplano.class);
-        startActivity(apertura);
-    }
-    public void pullovermancuerna(View view) {
-        Intent pullover = new Intent(this, Pulloverconmancuerna.class);
-        startActivity(pullover);
-    }
-    public void pressfrances(View view) {
-        Intent pressfrances = new Intent(this, Pressfrancesmancuerna.class);
-        startActivity(pressfrances);
-    }
-    public void dominadas(View view) {
-        Intent dominada = new Intent(this, Dominadas.class);
-        startActivity(dominada);
-    }
-    public void ehpushup(View view) {
-        Intent ehpushup = new Intent(this, Ehpushups.class);
-        startActivity(ehpushup);
-    }
-
-    public void ehpresshorizontal(View view) {
-        Intent ehpress = new Intent(this, Ehpresshorizontalalternado.class);
-        startActivity(ehpress);
-    }
-    public void remoaunamano(View view) {
-        Intent remo = new Intent(this, Remoaunamano.class);
-        startActivity(remo);
-    }
 }
